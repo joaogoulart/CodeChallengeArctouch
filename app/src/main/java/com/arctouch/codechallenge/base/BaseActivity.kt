c666ae05cdbeb21d72b20423ece58dbb9899ef98
João <joao.goulart@livetouch.com.br>
2018-03-25 19:20:38 -0300

    Fixed gradle

--- app/src/main/java/com/arctouch/codechallenge/base/BaseActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/base/BaseActivity.kt
@@ -5,14 +5,14 @@ import com.arctouch.codechallenge.api.TmdbApi
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 abstract class BaseActivity : AppCompatActivity() {
 
     protected val api: TmdbApi = Retrofit.Builder()
         .baseUrl(TmdbApi.URL)
         .client(OkHttpClient.Builder().build())
-        .addConverterFactory(MoshiConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
         .create(TmdbApi::class.java)
--- app/src/main/java/com/arctouch/codechallenge/model/Models.kt
+++ app/src/main/java/com/arctouch/codechallenge/model/Models.kt
@@ -1,16 +1,16 @@
 package com.arctouch.codechallenge.model
 
-import com.squareup.moshi.Json
+import com.google.gson.annotations.SerializedName
 
 data class GenreResponse(val genres: List<Genre>)
 
 data class Genre(val id: Int, val name: String)
 
 data class UpcomingMoviesResponse(
-    val page: Int,
-    val results: List<Movie>,
-    @Json(name = "total_pages") val totalPages: Int,
-    @Json(name = "total_results") val totalResults: Int
+        val page: Int,
+        val results: List<Movie>,
+        @SerializedName("total_pages") val totalPages: Int,
+        @SerializedName("total_results") val totalResults: Int
 )
 
 data class Movie(
@@ -18,8 +18,8 @@ data class Movie(
     val title: String,
     val overview: String?,
     val genres: List<Genre>?,
-    @Json(name = "genre_ids") val genreIds: List<Int>?,
-    @Json(name = "poster_path") val posterPath: String?,
-    @Json(name = "backdrop_path") val backdropPath: String?,
-    @Json(name = "release_date") val releaseDate: String?
+    @SerializedName("genre_ids") val genreIds: List<Int>?,
+    @SerializedName("poster_path") val posterPath: String?,
+    @SerializedName("backdrop_path") val backdropPath: String?,
+    @SerializedName("release_date") val releaseDate: String?
 )
